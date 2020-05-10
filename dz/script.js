70c1cc4fdd2e977cdd9efc96a6e2f81f22691a7a
AnnaSoh <64023238+AnnaSoh@users.noreply.github.com>
2020-05-11 01:39:42 +0200

    я пыталась

---
+++ dz/script.js
@@ -0,0 +1,37 @@
+class Student {
+    constructor(params) {
+       this.fullName = params.fullName;
+       this.university = params.university;
+       this.course = params.course;
+       this.photoUrl = params.photoUrl;
+    }
+  
+}
+
+const studentArr = [
+   {
+       fullName: 'Маша Иванова',
+       university: 'УГАТУ',
+       course: 2,
+       birthDate: new Date(2000, 0, 1),
+       photoUrl: '/photo1'
+   }
+];
+
+studentArr.forEach((item) => {
+    const student = new Student(item);
+    appendStudentBlock(student);
+});
+
+let stud = new Student();
+div1.addEventListener("click", stud);
+
+
+
+studentArr.forEach((item) => {
+    const student = new Student(item);
+    const studentBlock = appendStudentBlock(student);
+    studentBlock.addEventListener('click', (event) => {
+        openCard(student, event.currentTarget);
+    });
+});
